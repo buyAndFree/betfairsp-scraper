c382d12682f133e076eb84d9676ef42ad4ef4051
Kaan <33149606+kaanseven@users.noreply.github.com>
2018-07-09 21:38:14 +1000

    Add files via upload

---
+++ BetfairSP Scraper/BetfairAgent.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BetfairSP_Scraper
+{
+    class BetfairAgent
+    {
+        private const string betfairURI = "https://promo.betfair.com/betfairsp/prices/";
+        private static string rootDownloadDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        private HttpClient httpClient;
+        public BetfairAgent(HttpClient client)
+        {
+            httpClient = client;
+        }
+        /// <summary>
+        /// This method asynchronously downloads betfair startprice files and writes them to a csv file based on the input parameters given.
+        /// File types are determined by location, bet type, date, and type of race (greyhound/horses).
+        /// </summary>
+        /// <param name="location">Selected location (United Kingdom = uk, Ireland = ire, South Africa = rsa, Australia = aus, USA = usa).</param>
+        /// <param name="bettype">Selected betting type (win or place).</param>
+        /// <param name="greyhound">Download greyhound data only (location becomes irrelevant for this case).</param>
+        /// <param name="startDate">Starting date for selected downloads.</param>
+        /// <param name="endDate">Ending date for the selected downloads.</param>
+        /// <param name="token">Cancellation token, required if called by user to stop downloads.</param>
+        /// <returns></returns>
+        public async Task DownloadBetfairSPFiles(string location, string bettype, string greyhound, DateTime startDate, DateTime endDate, CancellationToken token)
+        {
+            if ((location == "uk" || location == "ire" || location == "aus" || location == "rsa" || location == "usa") 
+                && (bettype == "win" || bettype == "place") 
+                && (greyhound == "yes" || greyhound == "no") 
+                && (startDate <= endDate))
+            {
+                // The following strings appended to the root URL for downloading are dependent on betfair file naming conventions.
+                var baseLocalFileName = "dwbf";
+                if (greyhound == "yes")
+                {
+                    baseLocalFileName = baseLocalFileName + "greyhound";
+                }
+                else
+                {
+                    baseLocalFileName = baseLocalFileName + "prices" + location;
+                }
+                baseLocalFileName = baseLocalFileName + bettype;
+                string url = betfairURI + baseLocalFileName;
+                while (startDate <= endDate)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Downloading Task Cancelled!");
+                        return;
+                    }
+                    using (HttpResponseMessage response = await this.httpClient.GetAsync(url + (startDate.ToString("ddMMyyyy") + ".csv"), HttpCompletionOption.ResponseHeadersRead))
+                    using (Stream streamToReadFrom = await response.Content.ReadAsStreamAsync())
+                    {
+                        string fileToWriteTo = rootDownloadDirectory + (baseLocalFileName + startDate.ToString("ddMMyyyy") + ".csv");
+                        using (Stream streamToWriteTo = File.Open(fileToWriteTo, FileMode.Create))
+                        {
+                            await streamToReadFrom.CopyToAsync(streamToWriteTo);
+                        }
+                    }
+                    Console.WriteLine("<Downloaded " + startDate.ToString() + ">");
+                    startDate = startDate.AddDays(1);
+                }
+                Console.WriteLine("Downloading Complete!");
+            }
+            return;
+        }
+    }
+}
---
+++ BetfairSP Scraper/ScraperClass.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace BetfairSP_Scraper
+{
+    class ScraperClass
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        static void Main()
+        {
+            var cancelSource = new CancellationTokenSource();
+            var hcHandler = new HttpClientHandler() 
+            { 
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate 
+            };
+            var httpClient = new HttpClient(hcHandler);
+            var bfAgent = new BetfairAgent(httpClient);
+
+            Console.WriteLine("Betfair SP File Scraper");
+            Task downloadTask = null;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "start")
+                {
+                    if ((downloadTask != null) && (downloadTask.IsCompleted == false))
+                    {
+                        Console.WriteLine("Download Task is already running");
+                        continue;
+                    }
+                    Console.Write("Country (uk, ire, aus, usa, rsa): ");
+                    string country = Console.ReadLine().ToLower();
+                    if (!(country == "uk" || country == "ire" || country == "aus" || country == "usa" || country == "rsa"))
+                    {
+                        Console.WriteLine("Invalid Country");
+                        continue;
+                    }
+                    Console.Write("Bet type (win, place): ");
+                    string betType = Console.ReadLine().ToLower();
+                    if (!(betType == "win" || betType == "place"))
+                    {
+                        Console.WriteLine("Invalid Bettype");
+                        continue;
+                    }
+                    Console.Write("Greyhound Only? (yes, no): ");
+                    string greyhound = Console.ReadLine().ToLower();
+                    if (!(greyhound == "yes" || greyhound == "no"))
+                    {
+                        Console.WriteLine("Invalid Selection");
+                        continue;
+                    }
+                    Console.Write("Start Date (dd/mm/yyyy):");
+                    string startDateStr = Console.ReadLine();
+                    Console.Write("End Date (dd/mm/yyyy):");
+                    string endDateStr = Console.ReadLine();
+                    DateTime startDate;
+                    DateTime endDate;
+                    var enUS = new CultureInfo("en-US");
+                    if (DateTime.TryParseExact(startDateStr, "dd/MM/yyyy", enUS, DateTimeStyles.None, out startDate) && DateTime.TryParseExact(endDateStr, "dd/MM/yyyy", enUS, DateTimeStyles.None, out endDate))
+                    {
+                        if (startDate <= endDate)
+                        {
+                            downloadTask = bfAgent.DownloadBetfairSPFiles(country, betType, greyhound, startDate, endDate, cancelSource.Token);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: End Date is before Starting Date!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unable to parse dates");
+                        continue;
+                    }
+                }
+                else if (input == "exit")
+                {
+                    cancelSource.Cancel();
+                    Console.WriteLine("Exiting...");
+                    break;
+                }
+                else if (input == "stoptasks")
+                {
+                    Console.WriteLine("Stopping downloads...");
+                    cancelSource.Cancel();
+                    while (true)
+                    {
+                        Thread.Sleep(10);
+                        if (downloadTask.IsCanceled || downloadTask.IsCompleted || downloadTask.IsFaulted)
+                        {
+                            #region cancel the token source
+                            try
+                            {
+                                cancelSource.Cancel();   // If there's no timeout, the tokensource won't have been asked to cancel
+                            }
+                            catch (ObjectDisposedException)
+                            {
+                                // Already disposed
+                            }
+                            catch (AggregateException)
+                            {
+                                // AggregateException 
+                            }
+                            break;
+                            #endregion
+                        }
+                    }
+                    cancelSource = new CancellationTokenSource();
+                    //downloadTask = null;
+                    Console.WriteLine("Downloading stopped.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Command - Valid Commands are 'start', 'stoptasks', 'exit'");
+                }
+            }
+        }
+    }
+}
